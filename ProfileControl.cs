0513d390cc735820e91f3a1f6ef42a04650e7a8c
Thomas Elliott <thomas.elliott@nwcc.nsw.edu.au>
2020-07-27 10:22:28 +1000

    WIP1

---
+++ ProfileControl.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ProfileProgram {
+    class ProfileControl {
+        public static void ReadExistingProfile ( string fileDir, List<string> items ) {
+            List<string> profValues = File.ReadAllLines(fileDir).ToList();
+
+            for (int i = 0; i < items.Count; i++) {
+                Console.WriteLine(items[i] + profValues[i]);
+            }
+        }
+
+        public static void CreateNewProfile ( string fileDir, List<string> items ) {
+            List<string> profValues = new List<string>();
+
+            for (int i = 0; i < items.Count; i++) {
+
+                Console.WriteLine(items[i]);
+                string answer = Console.ReadLine();
+                profValues.Add(answer);
+                Console.WriteLine(answer);
+                Console.WriteLine();
+
+            }
+            
+            File.AppendAllLines(fileDir, profValues);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ProfileProgram
 {
@@ -6,7 +9,26 @@ namespace ProfileProgram
     {
         static void Main()
         {
-            
+            if ( !Directory.Exists("Profiles") ) {
+                Directory.CreateDirectory("Profiles");
+            }
+
+            List<string> profList = Directory.GetFiles("Profiles", "*", SearchOption.AllDirectories).Select(f => Path.GetFileName(f)).ToList();
+
+            List<string> items = new List<string> {
+                "Name: ",
+                "Favorite Color: ",
+                "Favorite Number: "
+            };
+
+            Console.WriteLine("Type new or existing username:");
+            string userInput = Console.ReadLine();
+
+            if ( File.Exists($"Profiles/{userInput}") ) {
+                ProfileControl.ReadExistingProfile( $"Profiles/{userInput}", items);
+            } else {
+                ProfileControl.CreateNewProfile( $"Profiles/{userInput}", items);
+            }
         }
     }
 }
